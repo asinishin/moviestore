537513b13f3ed2f72f17725b5149ad1c4a20cf95
Alex Sinishin <sinishin@gmail.com>
2017-03-11 21:49:47 -0800

    Initial

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,5 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.logger = Logger.new('log/braintree.log')
+Braintree::Configuration.merchant_id = 'rvyyj3d5vkqv6vxv'
+Braintree::Configuration.public_key = 'y874t7h9prth98qy'
+Braintree::Configuration.private_key = '5742349694b2c13130916a14586ff05b'
